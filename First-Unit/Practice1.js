84941ffd7a831ef55ecd10bb802f3497373ac8d6
chandro4D <chandroshakhor7@gmail.com>
2025-04-01 20:49:07 +0600

    Finish first unit

--- First-Unit/Practice1.js
+++ First-Unit/Practice1.js
@@ -1,2 +1,26 @@
+// practice problem 1:
 Positive : 12, 0, 3.5
 Negative : -5, -8 
+
+// practice problem 2:
+IntegerNumber:23, 56, 101, 42, 89
+EvenNumber:56, 42
+OddNumber:23, 101, 89, 19.69, 11.37
+
+// practice problem 3 :
+DecimalNumber:3.4, 2.75
+FractionNumber:7/8, 1/3
+
+// practice problem 4 :
+Total:1 = 90
+Total:2 = 54
+
+// practice problem 5 :
+Back = 36 Taka
+
+// practice problem 6 :
+TotalSpend = 1750
+
+// practice problem 7 :
+GetOnePerson = 56.25 Taka
+RemainingTaka = 2 Taka
