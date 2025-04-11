450b623f52e1a71560152c11b04530dd980edc2b
chandro4D <chandroshakhor7@gmail.com>
2025-04-11 23:04:55 +0600

    Complete unit1(practice2)

--- Second-Unit/Practice1.js
+++ Second-Unit/Practice1.js
@@ -1,8 +1,21 @@
 // practice problem 1:
+ const BirthYear = 2000;
+ 
 // practice problem 2:
+const BrotherSister = 2;
+
 // practice problem 3:
+const Students = 40;
+
 // practice problem 4:
+const marks = 72;
+
 // practice problem 5:
+const rooms =2;
+
 // practice problem 6:
+const studyHours = 12;
+
 // practice problem 7:
-// practice problem 8:
+const total = 0+1+7+5+9+6+2+6+4+7+0;
+console.log(total);
