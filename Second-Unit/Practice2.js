14ad3c48cf5c03f43c02b73f5915a642f84c58cf
chandro4D <chandroshakhor7@gmail.com>
2025-04-12 00:30:26 +0600

    complete unit1(practice2)

--- Second-Unit/Practice2.js
+++ Second-Unit/Practice2.js
@@ -1,7 +1,27 @@
 // practice problem 1:
+const age = 25;
+console.log(age);
+
 // practice problem 2:
+const booksToRead =13;
+console.log(booksToRead);
+
 // practice problem 3:
+const wakeUpTime = 6;
+console.log(wakeUpTime);
+
 // practice problem 4:
+const wastedTime = 4;
+console.log(wastedTime);
+
 // practice problem 5:
+const borrow = 2110;
+console.log(borrow);
+
 // practice problem 6:
+const goals = 5;
+console.log(goals);
+
 // practice problem 7:
+const pillow =2;
+console.log(pillow);
