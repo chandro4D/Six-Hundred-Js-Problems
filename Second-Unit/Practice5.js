b0fdd00af2edd7062c1704e5a5e00620672df9d9
chandro4D <chandroshakhor7@gmail.com>
2025-04-12 23:53:50 +0600

    complete unit 1

--- Second-Unit/Practice5.js
+++ Second-Unit/Practice5.js
@@ -1,8 +1,63 @@
+//Modules Or Remainder Operator (%)
+//Exponential (**)-->ES6
+
+
 // practice problem 1:
+const earnMoney = 100;
+const spendMoney = 40;
+const remainingMoney = earnMoney - spendMoney;
+console.log(remainingMoney);
+
 // practice problem 2:
+const totalPencil = 10;
+const atAtimeUses = 2;
+const numberOfUses = totalPencil / atAtimeUses;
+console.log(numberOfUses);
+
 // practice problem 3:
+const totalMoney = 60;
+const AtAtimeUses = 15;
+const NumberOfUses = totalMoney / AtAtimeUses;
+const RemainingMoney = totalMoney % AtAtimeUses;
+console.log(NumberOfUses,RemainingMoney);
+
 // practice problem 4:
+const first = "Hello";
+const second = "World";
+const AllText = first + second;
+console.log(AllText);
+
 // practice problem 5:
+const rice = 153;
+const pulse = 261;
+const total = rice + pulse;
+console.log(total);
+
 // practice problem 6:
+const TotalMoney = 500;
+const atAtimeSpend = 75;
+const numberOfSpending = TotalMoney / atAtimeSpend;
+const remainingmoney = TotalMoney % atAtimeSpend;
+console.log(numberOfSpending,remainingMoney);
+
 // practice problem 7:
+const quotient = 8/3;
+const remainder = 8%3;
+console.log(quotient,remainder);
+
 // practice problem 8:
+const Remainder = 50%9;
+console.log(Remainder);
+
+// practice problem 9:
+const Bangladesh = "Bangla" + "desh";
+console.log(Bangladesh);
+
+// practice problem 10:
+
+const totalRice = 480;
+const OneDayEat = 4;
+const totalDay = totalRice / OneDayEat;
+const TotalMonth = totalDay / 30;
+console.log(totalDay,TotalMonth);
+
