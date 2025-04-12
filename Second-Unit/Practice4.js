c4c63953b956014e73c76e7a8d1b1e1c1c7b44ff
chandro4D <chandroshakhor7@gmail.com>
2025-04-12 23:15:20 +0600

    complete unit2(practice:5)

---
+++ Second-Unit/Practice4.js
@@ -0,0 +1,30 @@
+// practice problem 1:
+const isHAppy = false;
+console.log(isHAppy);
+
+// practice problem 2:
+const isCrushMessage = false;
+console.log(isCrushMessage);
+
+// practice problem 3:
+const celebrating = true;
+console.log(celebrating);
+
+// practice problem 4:
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy,hasMoney,isHungry);
+
+// practice problem 5:
+const isStudyDone = false;
+console.log(isStudyDone)
+
+// practice problem 6:
+const isPhoneCharged = false;
+console.log(isPhoneCharged)
+
+// practice problem 7:
+const isDinnerDone  = false;
+console.log(isDinnerDone)
+
