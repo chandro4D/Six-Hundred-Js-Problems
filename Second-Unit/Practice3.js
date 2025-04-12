667eb16099ba57cbbe9abd013a4b340025e7c54f
chandro4D <chandroshakhor7@gmail.com>
2025-04-12 22:51:32 +0600

    completed unit2(practice:3)

---
+++ Second-Unit/Practice3.js
@@ -0,0 +1,27 @@
+// practice problem 1:
+const Name = "Chandra Shekhor Mondal";
+console.log(Name);
+
+// practice problem 2:
+const address = "2 No Koyra,Koyra,Khulna";
+console.log(address);
+
+// practice problem 3:
+const bestFriendCollege = "Govt. Haji Md. Mohsin College,Khulna";
+console.log(bestFriendCollege);
+
+// practice problem 4:
+const fan = "suchitra sen";
+console.log(fan);
+
+// practice problem 5:
+const movieOfTheYear = "Indrani";
+console.log(movieOfTheYear);
+
+// practice problem 6:
+const hobby = "Reading Book, Fishing & Gardening";
+console.log(hobby);
+
+// practice problem 7:
+const Color ="White";
+console.log(Color);
