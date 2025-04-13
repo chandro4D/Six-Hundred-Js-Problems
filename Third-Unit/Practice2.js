5c047a99975de1ad64cd5aeddc19c96a5aeccdbc
chandro4D <chandroshakhor7@gmail.com>
2025-04-13 09:24:05 +0600

    complete unit3(practice:2)

--- Third-Unit/Practice2.js
+++ Third-Unit/Practice2.js
@@ -1,10 +1,36 @@
 // practice problem 1:
+let age = 25;
+console.log(age);
+age = 26;
+console.log(age);
+
 // practice problem 2:
+let pocketMoney = 500;
+pocketMoney=150;
+console.log(pocketMoney);
+
 // practice problem 3:
+let studyTime = 4;
+studyTime=5;
+console.log(studyTime);
+
 // practice problem 4:
+let friendsCount = 3;
+friendsCount=1;
+console.log(friendsCount);
+
 // practice problem 5:
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
 // practice problem 6:
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+console.log(chairsCount);
+
 // practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+let playTime = 3;
+playTime = 2;
+console.log(playTime);
