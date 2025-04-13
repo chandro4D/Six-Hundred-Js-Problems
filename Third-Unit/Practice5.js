17623a54a094361a71b31355af70d5618ccbc3e8
chandro4D <chandroshakhor7@gmail.com>
2025-04-13 19:19:45 +0600

    complete unit-3

--- Third-Unit/Practice5.js
+++ Third-Unit/Practice5.js
@@ -1,10 +1,36 @@
 // practice problem 1:
+const number = parseInt("20") + 10;
+console.log(number);
+
 // practice problem 2:
+const number1 = parseInt("3.14159");
+console.log(number1); 
+
 // practice problem 3:
+const premik = parseInt("premikBoy");
+console.log(premik);
+
 // practice problem 4:
+const num1 = 3.456 ;
+const num2 =  2.789;
+const totalNum = num1 + num2 ;
+const total = parseFloat(totalNum.toFixed(2));
+console.log(total);
+
 // practice problem 5:
+const total1 = parseFloat("56.78") + parseFloat("12.34");
+console.log(total1);
+
 // practice problem 6:
+const firstNumber = parseFloat("10.5678");
+const secondNumber = parseFloat("5.4321");
+
+const firstNum = (firstNumber.toFixed(1));
+const secondNum = (secondNumber.toFixed(1));
+
+console.log(firstNum,secondNum);
+
 // practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const Number = parseFloat("42.4568954");
+const num =  (Number.toFixed(3));
+console.log(num);
