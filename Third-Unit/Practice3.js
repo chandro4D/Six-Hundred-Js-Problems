f970866d67018c7b9a2018feb5489079bdffb83c
chandro4D <chandroshakhor7@gmail.com>
2025-04-13 10:20:50 +0600

    complete unit3(practice:3)

--- Third-Unit/Practice3.js
+++ Third-Unit/Practice3.js
@@ -1,10 +1,57 @@
 // practice problem 1:
+
+//First T20 innings cricket score
+const score = 230;
+
+
+
 // practice problem 2:
+
+//radius
+//Of 
+//A Circle
+const radius = 20.5;
+
+
+
 // practice problem 3:
+
+/* 
+I like my
+white ,navy blue &
+black shirt most 
+*/
+const dress = 3;
+
+
+
 // practice problem 4:
+
+// Jhankar Mahabub vi send a gift of 100 tk for complete 600 js practice problem 
+const giftMoney = 100;
+
+
+
 // practice problem 5:
+
+/* 
+js book ==> 3 hours
+English Book ==> 2 hours
+sanchhita by our national poet ==> 2 hours
+*/
+studyTime = 7;
+
+
+
 // practice problem 6:
+
+// I send money to programming hero by Bkash for next level course
+// they get 6000 tk and 500 is service charge 
+const sendMoney = 6500;
+
+
+
 // practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+
+// Product price ==> 6000 & Delivery Charge ==> 500;
+const pay = 6500;
