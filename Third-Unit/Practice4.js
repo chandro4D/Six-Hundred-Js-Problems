3cc21060f8dc1e1aa2f0e1131eaa2123228cc94f
chandro4D <chandroshakhor7@gmail.com>
2025-04-13 18:55:13 +0600

    complete unit3(practice:4)

--- Third-Unit/Practice4.js
+++ Third-Unit/Practice4.js
@@ -1,10 +1,45 @@
 // practice problem 1:
+const favoriteFood = "Biryani";
+
 // practice problem 2:
+const city = "Chittagong";
+const City = "Dhaka";
+console.log(city,City);
+
 // practice problem 3:
+const userName = "Johan";
+const username = "Jovan";
+console.log(userName,username);
+
 // practice problem 4:
+const myHomeAddress = "2 No Koyra";
+
 // practice problem 5:
+// const super = 12;
+// console.log(super);
+/* 
+  I got a SyntaxError: Unexpected token 'super' error
+   because super is a reserved keyword in JavaScript.
+   We cannot use reserved keywords as variable names.
+*/
+
+
 // practice problem 6:
+// const class = "Bsc";
+// console.log(class);
+/* 
+  Here I also  got a SyntaxError: Unexpected token 'class' error
+   because super is a reserved keyword in JavaScript.
+   We cannot use reserved keywords as variable names.
+*/
+
+
+
 // practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const 2ndposition = "Liton das";
+console.log(2ndposition);
+/* 
+   Here I got SyntaxError: Invalid or unexpected token error 
+   because  javaScript now allowed any number before the variable name
+   & it is a rule of javaScript
+*/
