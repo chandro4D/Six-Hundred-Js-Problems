af86fc7b242a2c8cd2ea861f20b99b3a85a38667
chandro4D <chandroshakhor7@gmail.com>
2025-04-13 09:13:20 +0600

    complete unit3(practice:1)

---
+++ Third-Unit/Practice1.js
@@ -0,0 +1,24 @@
+// practice problem 1:
+const price = 555;
+console.log(typeof price);
+
+// practice problem 2:
+const learning = true;
+console.log(learning);
+console.log(typeof learning);
+
+// practice problem 3:
+const megaCity ="Dhaka";
+console.log(typeof megaCity);
+
+// practice problem 4:
+const temperature = 102.5;
+console.log(typeof temperature);
+
+// practice problem 5:
+const isHungry = true;
+console.log(typeof isHungry);
+
+// practice problem 6:
+const isRich = true;
+console.log(typeof isRich);
