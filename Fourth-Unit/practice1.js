d94e6ea4720cf0e0d689a297c7a925b79beb5463
chandro4D <chandroshakhor7@gmail.com>
2025-04-14 21:32:52 +0600

    complete unit4(practice:1)

---
+++ Fourth-Unit/practice1.js
@@ -0,0 +1,21 @@
+// practice problem 1:
+console.log(7>5);
+
+// practice problem 2:
+console.log(45>50);
+
+// practice problem 3:
+console.log(75 == 75);
+
+// practice problem 4:
+console.log(100<=95);
+
+// practice problem 5:
+console.log(10 != 7);
+
+// practice problem 6:
+console.log(20<=15);
+
+// practice problem 7:
+console.log(45>95);
+
