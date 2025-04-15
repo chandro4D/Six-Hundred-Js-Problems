a5fee32833c12fe26c164ee2340b6035f4064937
chandro4D <chandroshakhor7@gmail.com>
2025-04-15 18:19:32 +0600

    complete unit4(practice:2)

--- Fourth-Unit/practice2.js
+++ Fourth-Unit/practice2.js
@@ -1,10 +1,27 @@
 // practice problem 1:
+console.log(10<=15);
+
 // practice problem 2:
+console.log(33<=45);
+
 // practice problem 3:
+const Jambura = 200;
+const Kotbel = 120;
+console.log(Jambura < Kotbel);
+
 // practice problem 4:
+console.log(10 == 10);
+
 // practice problem 5:
+console.log(15 <20);
+
 // practice problem 6:
+const medrid = 2;
+const barcelona = 3;
+console.log(medrid < barcelona);
+
 // practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const myName = "Rahim";
+const friendName = "karim";
+console.log(myName == friendName);
+
