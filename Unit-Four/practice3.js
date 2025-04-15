ce26c163f8cae7a8367b74d9dca117bd0ccf08bb
chandro4D <chandroshakhor7@gmail.com>
2025-04-15 18:49:51 +0600

    complete unit-4

---
+++ Unit-Four/practice3.js
@@ -0,0 +1,37 @@
+// practice problem 1:
+console.log(50 == "50",50 === "50");
+//Output--> true , false
+
+// practice problem 2:
+console.log("JavaScript" == 'JavaScript',"JavaScript" === 'JavaScript');
+//Output--> true , true
+
+// practice problem 3:
+console.log(25 == "25", 25 === "25");
+//Output--> true , false
+
+// practice problem 4:
+console.log("apple" == "apple","apple" === "apple");
+//Output--> true , true
+
+// practice problem 5:
+console.log("test" == "Test", "test" === "Test");
+//Output--> false , false
+
+// practice problem 6:
+const variableX = 15;
+const variableY = "20";
+console.log(variableX != variableY,variableX !== variableY);
+//Output--> true , true
+
+// practice problem 7:
+const variableA = hello;
+const variableB = "Hello";
+console.log(variableA == variableB,variableA === variableB);
+/* 
+   when i try to assign const variableA = hello ,
+   javaScript treat it as a variable name(hello) ,so it give
+   me a error (hello is not defined).
+*/ 
+
+
