0b0353d3dc1fde1301c18b736d6c47544241567f
chandro4D <chandroshakhor7@gmail.com>
2025-04-16 10:44:04 +0600

    complete unit5(practice:2)

--- Unit-Five/practice2.js
+++ Unit-Five/practice2.js
@@ -1,10 +1,89 @@
 // practice problem 1:
+const age = 25;
+const hight = 65;
+if(age > 18 && hight > 60){
+    console.log("Push the Car");
+}
+else{
+    console.log("Seat in the car");
+}
+
 // practice problem 2:
+const math = 85;
+const eng = 98;
+if(math > 80 || eng > 85){
+    console.log("Eligible for admission");
+}
+else{
+    console.log("তোকে বিয়ে দিয়ে দিবো");
+}
+
 // practice problem 3:
+const GPA = 5;
+const monthlyIncome = 9500;
+if(GPA == 5 && monthlyIncome < 10000){
+console.log("He will get a scholarship");
+}
+else{
+    console.log("Pay Fee");
+}
+
 // practice problem 4:
+const Age = 25;
+const experience = 3;
+if(Age<30 && experience>2){
+    console.log("Eligible for giving exam");
+}
+else{
+    console.log("Not Eligible");
+}
+
 // practice problem 5:
+const egg = 15;
+const isChicken = false;
+if(egg>12 || isChicken == false){
+    console.log("Egg korma");
+}
+else{
+    console.log("Bread & Banana");
+}
+
 // practice problem 6:
+const temperature = 102;
+const isCough = true;
+if(temperature>100 || isCough == true){
+    console.log("Go to the Doctor");
+}
+else{
+    console.log("Sleep with blanket");
+}
+
 // practice problem 7:
+const attendance = 85;
+const isAllHomeWorkDone = true;
+if(attendance>80 || isAllHomeWorkDone == true){
+    console.log("Eligible For giving Exam");
+}
+else{
+    console.log("Auto Fail");
+}
+
 // practice problem 8:
+const isCurrentGone = true;
+const isPhoneCharged = false;
+if(isCurrentGone == true && isPhoneCharged == false){
+    console.log("I will seat for study");
+}
+else{
+    console.log("Play Video Game");
+}
+
 // practice problem 9:
-// practice problem 10:
+const shirtPrice = 1500;
+const isCupon = true;
+if(shirtPrice>1000 && isCupon == true){
+    console.log("20% Discount for you");
+}
+else{
+    console.log("Not Eligible For Any Kind Of Discount");
+}
