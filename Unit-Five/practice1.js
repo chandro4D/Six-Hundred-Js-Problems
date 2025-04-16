6fcc51254f45c2d6b2ef7a6c5715a70a5bbe41f5
chandro4D <chandroshakhor7@gmail.com>
2025-04-16 09:58:05 +0600

    complete unit5(practice:1)

--- Unit-Five/practice1.js
+++ Unit-Five/practice1.js
@@ -1,10 +1,53 @@
 // practice problem 1:
+const TimeToGoHome = 5;
+if(TimeToGoHome<6){
+    console.log("Will eat food");   
+}
+else{
+    console.log("Jhatar Bari");
+}
+
 // practice problem 2:
+const login = "successful";
+if(login == "successful"){
+    console.log("Welcome");   
+}
+else{
+    console.log("Get Lost");
+}
+
 // practice problem 3:
+const run= 5;
+if(run == 5){
+    console.log("Give Chocolate");
+}
+else{
+    "Weight Gain";
+}
+
 // practice problem 4:
+const number = 85;
+if(number>80){
+    console.log("Give Bike");
+}
+else{
+    console.log("Bike will no be given")
+}
+
 // practice problem 5:
+const movieShow = 10;
+if(movieShow<9){
+    console.log("Watch Movie");
+}
+else{
+    console.log("Sleep at home");
+}
+
 // practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const Temp = 35;
+if(Temp >= 30){
+    console.log("Run Ac");
+}
+else{
+    console.log("Sleep With a blanket");
+}
