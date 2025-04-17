8bf4f53602ec260cab793c47fb9f36d1c553450a
chandro4D <chandroshakhor7@gmail.com>
2025-04-17 23:38:51 +0600

    complete unit5(practice:3)

--- Unit-Five/practice3.js
+++ Unit-Five/practice3.js
@@ -1,4 +1,19 @@
 // practice problem 1:
+const price = 4500;
+if(price >= 6000){
+    const discounted_price = price/100 * 15 ;
+    const pay = price - discounted_price;
+    console.log(pay);
+}
+else if(price >= 3000){
+    const discounted_price = price/100 * 5 ;
+    const pay = price - discounted_price;
+    console.log(pay);
+}
+else{
+    console.log(price);
+}
+
 // practice problem 2:
 // practice problem 3:
 // practice problem 4:
