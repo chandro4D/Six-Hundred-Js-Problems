37ae4e659d442f591a748643063135adfba887cf
chandro4D <chandroshakhor7@gmail.com>
2025-04-21 14:51:08 +0600

    Complete unit-6

--- Unit-Six/practice4.js
+++ Unit-Six/practice4.js
@@ -1,10 +1,34 @@
 // practice problem 1:
+const fruits = ["Mango","Apple","Banana","Litchi"];
+if(fruits.includes("Mango")){
+    console.log("Have Mango");
+}
+else{
+    console.log("There is no mango,please get up the trees");
+}
+
 // practice problem 2:
+const names = ["Babul","Alif","Choton"];
+console.log(names.indexOf("Babul"));
+
 // practice problem 3:
+const friends = ["Rimon","Rifat","Rajib"];
+console.log(friends.indexOf("Rifat"));
+
 // practice problem 4:
+const cites = ["Dhaka","Chittagong","Sylhet"];
+cites.push("rajshahi");
+console.log(cites.includes("Rajshahi"));
+
 // practice problem 5:
+const elements = ["Dighi","Megh","Brishte","Borsha"];
+if(elements.includes("Brishte")){
+   console.log("I Need Umberalla");
+}
+else{
+    console.log("No Rain No Pain");
+}
+
 // practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const games = ["Football","Cricket","Volleyball"];
+console.log(games.includes("Badminton"));
