dd3b4a32b6dcec0c96a10976be38ea04765c727e
chandro4D <chandroshakhor7@gmail.com>
2025-04-21 12:12:56 +0600

    Complete unit6(Practice:3)

--- Unit-Six/practice3.js
+++ Unit-Six/practice3.js
@@ -1,10 +1,25 @@
 // practice problem 1:
+const numbers = [10,20,30,40,50];
+numbers.push(60);
+console.log(numbers);
+
 // practice problem 2:
+const names = ["Sajib","Sagor","Shakib","Sohel"];
+names.push("Sumon");
+console.log(names);
+
 // practice problem 3:
+const games = ["Free Fire","Chess"];
+games.pop();
+console.log(games);
+
 // practice problem 4:
+const Numbers = [24,26,48,60];
+Numbers.unshift(12);
+console.log(Numbers);
+
 // practice problem 5:
-// practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const books = ["Joloputrro","Koshbi","Pather pachili","Sonchita","How to talk to anyone"];
+books.shift();
+console.log(books);
+
