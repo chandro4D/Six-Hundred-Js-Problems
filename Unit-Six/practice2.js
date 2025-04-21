f45804c288499fb537062cf852df8c31d3d50af2
chandro4D <chandroshakhor7@gmail.com>
2025-04-21 08:59:29 +0600

    conplete unit6(Practice:2)

--- Unit-Six/practice2.js
+++ Unit-Six/practice2.js
@@ -1,10 +1,31 @@
 // practice problem 1:
+const friends = ["Sujit","Alvi","Akibur","Sakibul","Niloy","Krishna","Prianka","Sabina","Tumpa","Rahul"];
+console.log(friends[3]);
+
 // practice problem 2:
+const books = ["Joloputrro","Koshbi","Pather pachili","Sonchita","How to talk to anyone","Shojon Badiare ghat","Othelo"];
+console.log(books[5]);
+
 // practice problem 3:
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+numbers[7]=30;
+console.log(numbers);
+
 // practice problem 4:
+const games = ["Free Fire","Pabg","Candi Crash","Temple Run"];
+games[2]="Subway Sarfar";
+console.log(games);
+
 // practice problem 5:
+const faMember = ["Narayon Chandra Mondal","Bithika Rani mondal","Chandra Shekhor Mondal","Binhu Shekhor Mondal","Montu Mondal","Lolita rani Mondal","Nabodip Kumer Mondal"];
+console.log(faMember[3]);
+
 // practice problem 6:
+const country = ["Napel","India","Srilanka","Maldip","Germany","America","Japan"];
+console.log(country[4]);
+
 // practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const elements = ["pen","Pencil","Table lamp","Laptop","Keyboard"];
+console.log(elements[7]);
+// Output is Undefined because there is no assigned value for the index 7....
+
