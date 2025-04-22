fe8f9092d40eb840c5f5d849a72af6cc6d6dca36
chandro4D <chandroshakhor7@gmail.com>
2025-04-22 22:58:40 +0600

    Complete unit7(practice:1)

--- Unit-Seven/practice1.js
+++ Unit-Seven/practice1.js
@@ -1,10 +1,30 @@
 // practice problem 1:
+const fevSubject = ["Bangla","English","Math","Biology","CSE"];
+
+for(const subject of fevSubject){
+    console.log(subject);
+}
+
 // practice problem 2:
+const fevFood = ["Chicken Fry","Sweet","Briyani","Fish Fry"];
+for(const food of fevFood){
+    console.log(food);
+}
+
 // practice problem 3:
+const allBirthYear = [1964,1980,2000,2013];
+for(const birthYear of allBirthYear){
+    console.log(birthYear);
+}
+
 // practice problem 4:
+const allTeamMember = ["Chandra Shekhor","alvi","Akibur","Bindu","Nabodip","NIloy","Sujit","Krishna","Shakib","Arju","Yeasir"];
+for(const member of allTeamMember){
+    console.log(member);
+}
+
 // practice problem 5:
-// practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const allExamDate = ["01/06/25","03/06/25","07/06/25","10/06/25","12/06/25","15/06/25","20/06/25","23/06/25","25/06/25"];
+for(const examDate of allExamDate){
+    console.log(examDate);
+}
