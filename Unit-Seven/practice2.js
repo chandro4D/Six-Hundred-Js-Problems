fccf167c2f12899d1c5565bc2422bf7cd40b600a
chandro4D <chandroshakhor7@gmail.com>
2025-04-23 23:48:29 +0600

    Complete unit7(Practice:2)

--- Unit-Seven/practice2.js
+++ Unit-Seven/practice2.js
@@ -1,10 +1,34 @@
 // practice problem 1:
+let ProPractice = 0;
+while(ProPractice < 10){
+    console.log("I Will Practice Programming For 3 Hours Every Day")
+    ProPractice += 1;
+}
+
 // practice problem 2:
+let num = 1;
+while(num <= 20){
+    console.log(num);
+    num += 1;
+}
+
 // practice problem 3:
+let num1 = 50;
+while(num1 <= 100){
+    console.log(num1);
+    num1 += 1;
+}
+
 // practice problem 4:
+let num2 = 1;
+while(num2 <= 10){
+    console.log(num2 * 3);
+    num2 += 1;
+}
+
 // practice problem 5:
-// practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+let num3 = 111;
+while(num3 >= 110){
+    console.log(num3 / 2);
+    num3 -= 1;
+}
