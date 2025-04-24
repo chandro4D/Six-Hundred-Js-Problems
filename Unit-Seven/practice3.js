c6134f2de8ac50ddf940b2a887bf32879f9cf014
chandro4D <chandroshakhor7@gmail.com>
2025-04-24 23:30:42 +0600

    Complete unit7(practice:3)

--- Unit-Seven/practice3.js
+++ Unit-Seven/practice3.js
@@ -1,10 +1,42 @@
 // practice problem 1:
+let num = 50;
+while(num <= 100){
+    console.log(num);
+    num++;
+}
+
 // practice problem 2:
+let num1 = 5;
+let sum = 0;
+while(num1 <= 15){
+ sum = sum + num1;
+ num1++;
+}
+console.log(sum);
+
+
 // practice problem 3:
+let rollNum = 1;
+let sum1 = 0;
+while(rollNum <= 50){
+    console.log(rollNum);
+    sum1 = sum1 + rollNum;
+    rollNum++;
+}
+console.log(sum1);
+let num3 = 21;
+let sum3 = 0;
+while(num3 <= 50){
+    sum3 = sum3 + num3;
+    console.log(sum3);
+    num3++;
+}
+
 // practice problem 4:
-// practice problem 5:
-// practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+let num4 = 20;
+let sum4 = 0;
+while(num4 <= 40){
+    sum4 = sum4 + num4;
+    num4++;
+}
+console.log(sum4);
