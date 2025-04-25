aca024227d8c76c19ae88bba8db76e5b5cedb322
chandro4D <chandroshakhor7@gmail.com>
2025-04-25 12:55:43 +0600

    complete unit-7

--- Unit-Seven/Practice6.js
+++ Unit-Seven/Practice6.js
@@ -1,10 +1,48 @@
 // practice problem 1:
+for(let num = 1; num <= 30; num++){
+    if(num > 15){
+        continue;
+    }
+    console.log(num);
+}
+
 // practice problem 2:
+for(let num1 = 1; num1 <= 40; num1++){
+    if(num1 % 7 == 0){
+        continue;
+    }
+    console.log(num1);
+}
+
 // practice problem 3:
+for(let num2 = 1; num2 <= 15; num2++){
+    if(num2 == 9){
+        continue;
+    }
+    console.log(num2);
+}
+
+
 // practice problem 4:
+for(let num3 = 1; num3 <= 20; num3++){
+    if(num3 == 12){
+        continue;
+    }
+    console.log(num3);
+}
+
 // practice problem 5:
+for(let num4 = 1; num4 <= 25; num4++){
+    if(num4 % 3 == 0 ){
+        continue;
+    }
+    console.log(num4);
+}
+
 // practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+for(let num5 = 91; num5 <= 120; num5++){
+    if(num5 % 10 == 0){
+        break;
+    }
+    console.log(num5);
+}
