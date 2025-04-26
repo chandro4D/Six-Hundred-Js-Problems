044a8d4e61332b7570e6f06704f1285a1951051b
chandro4D <chandroshakhor7@gmail.com>
2025-04-26 23:25:47 +0600

    complete unit8(Practice:1)

--- Unit-Eight/Practice1.js
+++ Unit-Eight/Practice1.js
@@ -1,10 +1,56 @@
 // practice problem 1:
+const teacher = {
+    name: "Narayon Chandra Mondal",
+    subject: "English",
+    age: 45,
+    address:"2 No Koyra",
+    isSchoolTeacher: false
+}
+
 // practice problem 2:
+const tree = {
+    treeName: "Mango",
+    hight: "50 feet",
+    age: 20,
+    fruitName : "Mango"
+}
+
 // practice problem 3:
+const cat = {
+    name: "pusi cat",
+    color: "White & Black",
+    age: 5,
+    fevFood: "Milk"
+}
+
 // practice problem 4:
+const father = {
+    Name: "Narayon Chandra Mondal",
+    occupation: "Teacher",
+    age: 45,
+    numOfSons: 2
+}
+
 // practice problem 5:
+const bike = {
+    BrandName: "Suzuki",
+    TireNum: 2,
+    Speed: "100 ph",
+    Price: 250000
+}
+
 // practice problem 6:
+const bird = {
+    name:"Myna",
+    color:"Black",
+    food: "Rice",
+}
+
 // practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const laptop = {
+    BrandName: "Asus",
+    Processor: "Intel core i5",
+    Ram: "8 GB",
+    DisplaySize: "16 Inch"
+}
+
