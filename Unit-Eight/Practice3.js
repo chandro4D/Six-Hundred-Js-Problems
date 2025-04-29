4d4050eec7e82360e8ecb4a173affc801869b03c
chandro4D <chandroshakhor7@gmail.com>
2025-04-29 23:09:42 +0600

    Complete unit8(Practice:3)

--- Unit-Eight/Practice3.js
+++ Unit-Eight/Practice3.js
@@ -1,10 +1,70 @@
 // practice problem 1:
+const book = {
+    bookName: "Marhaba JavaScripte Maro Thaba",
+    authorName: "Jhankar Mahbub",
+    price: 638
+}
+console.log(Object.keys(book), Object.values(book));
+
 // practice problem 2:
+const article = {
+    title: "Learning Js",
+    category: "Programming"
+}
+console.log(Object.keys(article).includes("author"));
+
 // practice problem 3:
+const laptop = {
+    brand: "Dell",
+    model: "Inspiron",
+    price: 45000
+}
+
+
 // practice problem 4:
+const phone = {
+    brand: "Samsung",
+    model: "Galaxy s21",
+    price: 85000
+}
+
 // practice problem 5:
+const bike = {
+    brand: "Hero",
+    price: 120000,
+    model: "Splendor"
+}
+
 // practice problem 6:
+const books = {
+    book1: "Harry Potter",
+    book2: "The Hobbit",
+    book3: "Game of Thrones"
+}
+
 // practice problem 7:
+const numbers = {
+    a: 10,
+    b: 20,
+    c: 30,
+    d: 40
+}
+
 // practice problem 8:
+const player = {
+    name: "Messi",
+    team: "Argentina",
+    goals: 91
+}
+
 // practice problem 9:
+const building = {
+    floors: 10,
+    address:{
+        street:"Main Road",
+        city: "Dhaka"
+    },
+    type: "Commercial"
+
+}
 // practice problem 10:
