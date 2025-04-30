ee403732954fcf32f2540fb1aca7309b2bf0a85a
chandro4D <chandroshakhor7@gmail.com>
2025-04-30 18:38:04 +0600

    Complete Unit-8

--- Unit-Eight/Practice4.js
+++ Unit-Eight/Practice4.js
@@ -1,10 +1,58 @@
 // practice problem 1:
+const headphone = {
+    brand: "Sony",
+    price: 3000,
+    color: "Red"
+}
+Object.freeze(headphone);
+headphone.isBluetooth = false;
+console.log(headphone);
+
 // practice problem 2:
+const player = {
+    name: "Messi",
+    goals: 800,
+    club: "Inter Miami"
+}
+Object.freeze(player);
+player.country = "Argentina";
+console.log(player);
+
 // practice problem 3:
+const book = {
+    title: "Harry Porter",
+    author: "JK Rowling",
+    pages: 500
+}
+Object.seal(book);
+book.author = "Chandra Shekhor Mondal";
+console.log(book);
+
 // practice problem 4:
+const gadget = {
+    name: "iPhone",
+    price: 120000,
+    color: "Black"
+}
+delete gadget.price;
+console.log(gadget);
+
 // practice problem 5:
+const animal = {
+    name: "Tiger",
+    location: "Sundarban"
+}
+Object.freeze(animal);
+animal.location = "Khulna";
+console.log(animal);
+
 // practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const food = {
+    name: "Pizza",
+    price: 500,
+    size: "Large"
+}
+Object.seal(food);
+food.location = "Khulna";
+food.price = 1000;
+console.log(food);
