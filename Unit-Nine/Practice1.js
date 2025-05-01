d7a91e5601d56d6ce44c1ee134d32a5f0486a8aa
chandro4D <chandroshakhor7@gmail.com>
2025-05-01 10:39:29 +0600

    Complete Unit9(Practice:1)

--- Unit-Nine/Practice1.js
+++ Unit-Nine/Practice1.js
@@ -1,10 +1,49 @@
 // practice problem 1:
+function juiceEat(){
+    console.log("Take This Lemon Juice")
+}
+juiceEat();
+
 // practice problem 2:
+function myRoutine(){
+    console.log("1 Hour English");
+    console.log("2 Hour Top Priority Task");
+    console.log("1 Hour Cook");
+    console.log("2 Hour Web ");
+    console.log("1 Hour Social Media");
+}
+myRoutine();
+
 // practice problem 3:
+function familyMemberName(){
+    console.log("Narayon Chandra Mondal\nBithika Rani Mondal\nChandra Shekhor Mondal\nBindu Shekhor Mondal");
+}
+familyMemberName();
+
 // practice problem 4:
+function taskDone(){
+    console.log("English Learning\nWeb Development\nRead Js Book\nRead A Poem Book\nGerman Learning");
+}
+taskDone();
+
 // practice problem 5:
+function greetMe(){
+    console.log("Good Morning! Have A Great Day");
+}
+greetMe();
+
 // practice problem 6:
+function getFavFriends(){
+    console.log("Sujit Kumar\nRomel Mistry\nBayzid Hassan\nBishojit Roy");
+}
+getFavFriends();
+
 // practice problem 7:
+function introduceMyself(){
+    console.log("Name:Chandra Shekhor Mondal\nAge:24\nAddress:2 No Koyra,Koyra,Khulna\nPhone:01759626470\nHight:5.5''\nFavFood:Biryani");
+}
+introduceMyself();
+
 // practice problem 8:
 // practice problem 9:
 // practice problem 10:
