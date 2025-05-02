7453fe9e1dd6f8899f72bd2fe3d1f5d16d5a347b
chandro4D <chandroshakhor7@gmail.com>
2025-05-02 19:01:52 +0600

    Complete unit9(Practice:2)

--- Unit-Nine/Practice2.js
+++ Unit-Nine/Practice2.js
@@ -1,10 +1,58 @@
 // practice problem 1:
+function fatherSonAge(fatherAge,SonAge){
+    console.log("Total Age:",fatherAge+SonAge);
+}
+fatherSonAge(35,25);
+
+
 // practice problem 2:
+function cal(num1,num2){
+    console.log(num1*num2);
+}
+cal(20,15);
+
 // practice problem 3:
+function threeSubMarks(first1,second1,third1){
+    console.log("Your Three Subject Marks is:",first1+second1+third1);
+}
+threeSubMarks(80,80,90);
+
 // practice problem 4:
+function ageCalculate(yourAge){
+    let age = 2025-yourAge;
+    console.log("Your Age Is:",age);
+}
+ageCalculate(2000);
+
 // practice problem 5:
+function takaForBuy(totalTaka){
+    let numOfLaw = totalTaka/35;
+    console.log("You Can Buy:",numOfLaw);
+}
+takaForBuy(70);
+
 // practice problem 6:
+function AverageOfFourNum(num1,num2,num3,num4){
+    console.log("Average Of Four Number is:",(num1+num2+num3+num4)/4);
+}
+AverageOfFourNum(25,25,25,25);
+
 // practice problem 7:
+function SellingPrice(BuyingPrice){
+    console.log("The Selling Price Of this Product is:",BuyingPrice+250);
+}
+SellingPrice(200);
+
 // practice problem 8:
+function hundredYear(yourAge){
+    console.log("The Year When Your Age is 100 is:",yourAge+100);
+}
+hundredYear(2000);
+
 // practice problem 9:
+function phoneUseTimeFor1Month(UseADay){
+    console.log("You Use Phone",UseADay*30,"Hours A Month");
+}
+phoneUseTimeFor1Month(2);
+
 // practice problem 10:
