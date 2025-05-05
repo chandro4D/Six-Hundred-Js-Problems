8013d2a2c138cf20f542b110e06ef5f32dee61b6
chandro4D <chandroshakhor7@gmail.com>
2025-05-05 23:47:33 +0600

    Complete unit-9

--- Unit-Nine/Practice5.js
+++ Unit-Nine/Practice5.js
@@ -1,4 +1,17 @@
 // practice problem 1:
+const num = [5,15,8,7];
+let sum = 0;
+function addOddOnly(number){
+    for(const addOddNumber of num){
+        if(addOddNumber%2 != 0){
+            sum = sum + addOddNumber;
+        }
+    }
+    return sum;
+}
+
+console.log(addOddOnly(num));
+
 // practice problem 2:
 // practice problem 3:
 // practice problem 4:
