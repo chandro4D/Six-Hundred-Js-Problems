b95557acc81145daacde685012f85e3f0da12a59
chandro4D <chandroshakhor7@gmail.com>
2025-05-06 23:01:11 +0600

    Complete Unit10(Practice:1)

--- Unit-Ten/Practice1.js
+++ Unit-Ten/Practice1.js
@@ -1,10 +1,48 @@
 // practice problem 1:
+function mileToYard(mile){
+    const yard = mile*1760;
+    return yard;
+}
+console.log(mileToYard(13));
+
 // practice problem 2:
+function kiloWattPerHourToKiloCalory(kiloWattPerHour){
+    const KiloCalory = kiloWattPerHour*860;
+    return KiloCalory;
+}
+console.log(kiloWattPerHourToKiloCalory(10));
+
 // practice problem 3:
+function hourToSecond(hour){
+    const second = hour*60*60;
+    return second;
+}
+console.log(hourToSecond(10));
+
 // practice problem 4:
+function cmToMeter(cm){
+    const meter = cm/100;
+    return meter;
+}
+console.log(cmToMeter(1000));
+
 // practice problem 5:
+function inchToCm(inch){
+    const cm = inch*2.54;
+    return cm;
+}
+console.log(inchToCm(10));
+
 // practice problem 6:
+function poundToKg(pound){
+    const Kg = pound*0.453;
+    return Kg;
+}
+console.log(poundToKg(10));
+
 // practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+function yardToMeter(Yard){
+    Meter = Yard*0.91;
+    return Meter;
+}
+console.log(yardToMeter(10));
