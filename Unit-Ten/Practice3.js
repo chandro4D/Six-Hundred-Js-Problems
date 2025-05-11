367813066ba555945709fbeda3891c395ae8c0e1
chandro4D <chandroshakhor7@gmail.com>
2025-05-11 15:53:08 +0600

    complete unit-10(practice:3)-1

--- Unit-Ten/Practice3.js
+++ Unit-Ten/Practice3.js
@@ -1,4 +1,20 @@
 // practice problem 1:
+const arr = [12,10,15,17];
+let sum = 0;
+let avg = 0;
+function isEvenNumber(array){
+    for(const Arr of array){
+        if(Arr % 2 !== 0){
+            sum += Arr;
+            let arraylength =array.length;
+            avg += sum/arraylength;
+        }
+        
+    }
+    console.log(avg);
+}
+isEvenNumber(arr);
+
 // practice problem 2:
 // practice problem 3:
 // practice problem 4:
