b1ce60dfb38c167cf82f3f08014da9fe2d118d33
chandro4D <chandroshakhor7@gmail.com>
2025-05-15 22:42:13 +0600

    unit10-all-complete

--- Unit-Ten/Practice4.js
+++ Unit-Ten/Practice4.js
@@ -10,3 +10,11 @@ function noDuplicate(arrOfNumbers){
 }
 const arr = [1,5,61,5,87,7,8,81,61];
 console.log(noDuplicate(arr));
+
+let a = 5;
+let b = 7;
+console.log(a,b);
+const temp = a;
+a=b;
+b=temp;
+console.log(a,b);
