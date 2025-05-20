24deae887265339a14ae8e0d85cf65bdac76b195
chandro4D <chandroshakhor7@gmail.com>
2025-05-20 22:43:34 +0600

    complete unit-11(Pr-1)

--- Unit-Eleven/Practice2.js
+++ Unit-Eleven/Practice2.js
@@ -1,4 +1,8 @@
 // practice problem 1:
+const num1 = 5000;
+const num2 = 2000;
+const subsTraction= num1 - num2;
+console.log(`The Difference Between ${num1} and ${num2} is ${subsTraction}`); 
 // practice problem 2:
 // practice problem 3:
 // practice problem 4:
