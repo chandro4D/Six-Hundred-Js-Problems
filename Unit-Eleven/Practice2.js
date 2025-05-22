c3f9a00fcb6e5dba42da26ee99339c51bd9cc746
chandro4D <chandroshakhor7@gmail.com>
2025-05-22 23:51:02 +0600

    complete Unit-11(Practice:2)

--- Unit-Eleven/Practice2.js
+++ Unit-Eleven/Practice2.js
@@ -16,11 +16,36 @@ Name: ${employee.name}
 Age:  ${employee.age}
 Salary: ${employee.salary}
      `);
+
+
 // practice problem 3:
+const fruits = ["Banana","Apple","Mongo","Watermelon"];
+console.log(`My Third Favorite Fruit is--> ${fruits[2]}`);
+
 // practice problem 4:
+const a=50;
+const b=10;
+const div = a-b;
+console.log(`The Division of a and b is ${div}`);
+
 // practice problem 5:
+const person = {firstName: "Amit",lastName:"Kumar"};
+console.log(`Full Name: ${person.firstName} ${person.lastName}`);
+
 // practice problem 6:
+const animals = ["Cat","Dog","Elephant"];
+console.log(`My Favorite Animals Are ${animals[0]},${animals[1]},${animals[2]}`);
+
 // practice problem 7:
+const students ={
+    name:"Chandra Shekhor Mondal",
+    age:25,
+    result:[70,80,90]
+}
+const all = `Name=${students.name}
+Age=${students.age} 
+Average Mark Is ${(students.result[0]+students.result[1]+students.result[2])/3}`
+console.log(all);
 // practice problem 8:
 // practice problem 9:
 // practice problem 10:
