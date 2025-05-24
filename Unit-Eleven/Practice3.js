56c9d7d5f1b1305658bf721806f21e704c4f2bed
chandro4D <chandroshakhor7@gmail.com>
2025-05-24 14:06:51 +0600

    Complete Unit-11

--- Unit-Eleven/Practice3.js
+++ Unit-Eleven/Practice3.js
@@ -4,14 +4,27 @@ const FirstFriend = () => friends[0]
 console.log(FirstFriend());
 
 // practice problem 2:
-const mulOfThreeNum = (num1,num2,num3) => num1*num2*num3
+const mulOfThreeNum = (num1,num2,num3) => num1*num2*num3;
 console.log(mulOfThreeNum(1,2,3));
 
 // practice problem 3:
+const unKnown = () => "unknown" ;
+console.log(unKnown());
+
 // practice problem 4:
+const nameTaka = {
+    name: "Chandra Shekhor Mondal",
+    taka:350
+}
+const division = (obj) => obj.taka/5 ;
+console.log(division(nameTaka));
+
+
 // practice problem 5:
+const arrNum = [70,12,20,10,30,68];
+const addFirstAndLastNum = (arr) => arr[0] + arr[arr.length -1];
+console.log(addFirstAndLastNum(arrNum));
+
 // practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const addTwoNum = (num1 = 10,num2 =5) => num1+num2
+console.log(addTwoNum());
