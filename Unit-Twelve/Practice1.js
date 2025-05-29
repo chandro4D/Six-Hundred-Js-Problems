8534b3d21daa7cd5ff6a0ad78c0cf327c9bb3606
chandro4D <chandroshakhor7@gmail.com>
2025-05-29 23:43:20 +0600

    complete unit-12(Practice:1-5)

--- Unit-Twelve/Practice1.js
+++ Unit-Twelve/Practice1.js
@@ -15,10 +15,25 @@ const item = {
 }
 const {Phone,Price}=item;
 console.log("Phone:", Phone,",","Price:",Price);
+
 // practice problem 3:
+const colors = ["red","blue","green","yellow"];
+const [first, second] = colors;
+console.log(first,second);
 
 // practice problem 4:
+const threeNum = [11,68,22];
+const [,seconD] = threeNum;
+const Second = seconD;
+console.log(Second);
+
 // practice problem 5:
+const digits = [2,4,6,8];
+const [two,,,eight] = digits;
+const Two = two;
+const Eight = eight;
+console.log(Two,Eight);
+
 // practice problem 6:
 // practice problem 7:
 // practice problem 8:
