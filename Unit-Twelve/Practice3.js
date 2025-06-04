69599e0ad6fccaf3522bbf908dc31f19c468f2ea
chandro4D <chandroshakhor7@gmail.com>
2025-06-04 23:03:32 +0600

    Complete unit12(PR:1-3)

---
+++ Unit-Twelve/Practice3.js
@@ -0,0 +1,25 @@
+// practice problem 1:
+const technologies= ["condition","array","loop"];
+const newArray = ["variable","Object", ...technologies];
+console.log(newArray);
+
+// practice problem 2:
+const fruits = [ "Apple", "Banana", "Mango"];
+const myFruits = [ ...fruits, "Papaya","Orange"];
+console.log(myFruits);
+
+// practice problem 3:
+const frontEnd = ["JavaScript"];
+const backEnd = ["Node.Js"];
+const dataBase = ["MongoDB"];
+const fullStackDev = [...frontEnd,...backEnd,...dataBase];
+console.log(fullStackDev);
+
+// practice problem 4:
+
+// practice problem 5:
+// practice problem 6:
+// practice problem 7:
+// practice problem 8:
+// practice problem 9:
+// practice problem 10:
