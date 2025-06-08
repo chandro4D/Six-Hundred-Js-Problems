89ccc02552abca3edb8e23fbc6e3956adf22ac45
chandro4D <chandroshakhor7@gmail.com>
2025-06-09 00:38:50 +0600

    Complete unit-13(Practice:2)

--- Unit-Therteen/Practice2.js
+++ Unit-Therteen/Practice2.js
@@ -1,10 +1,20 @@
 // practice problem 1:
+const numbers = [100,120,10,20,50,300];
+const bigNum = numbers.some(num => num > 100);
+console.log(bigNum);
+
 // practice problem 2:
+const array = [5,10,15,20,25,63];
+const isDivByFive = array.every(Num => Num % 5 == 0);
+console.log(isDivByFive);
+
 // practice problem 3:
+const words = ["Chandra","Bindu","Nabodip","Bithika","Narayon","hello"];
+const isEqualHello = words.some(word => word == "hello" );
+console.log(isEqualHello);
+
 // practice problem 4:
-// practice problem 5:
-// practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const ages = [19,20,40,30,13];
+const more = ages.every(age => age>18);
+console.log(more);
+
