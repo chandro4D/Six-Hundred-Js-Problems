90437fbdb9ef8b236f1a80f5bc6060f5633d3085
chandro4D <chandroshakhor7@gmail.com>
2025-06-12 11:18:33 +0600

    Complete unit13(Practice:4)

--- Unit-Therteen/Practice4.js
+++ Unit-Therteen/Practice4.js
@@ -1,10 +1,23 @@
 // practice problem 1:
+const numbers = [50,12,25,8,15];
+numbers.sort((num1,num2)=> num1 -num2);
+console.log(numbers);
+
 // practice problem 2:
+const Numbers = [13,2,45,9,6];
+Numbers.sort((num1,num2) => num2 - num1);
+console.log(Numbers);
+
 // practice problem 3:
+const friends = [
+    {name : "Ali",age : 29},
+    {name : "Sara",age : 22},
+    {name : "Tariq",age :35}
+];
+friends.sort((name1,name2) => name1.age - name2.age);
+console.log(friends);
+
 // practice problem 4:
-// practice problem 5:
-// practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const names = ["Nabil","Zubayer","Sarwar","Delwar"];
+names.sort();
+console.log(names);
