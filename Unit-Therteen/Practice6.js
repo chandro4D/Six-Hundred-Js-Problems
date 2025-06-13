56f1be6848556c743b5049d510130ddaf51fb3bd
chandro4D <chandroshakhor7@gmail.com>
2025-06-13 07:46:27 +0600

    Complete unit->13

--- Unit-Therteen/Practice6.js
+++ Unit-Therteen/Practice6.js
@@ -1,10 +1,20 @@
 // practice problem 1:
+const fruits = ["Apple","Banana","Cherry","Date"];
+const someFruits = fruits.splice(1,2);
+console.log(someFruits);
+
 // practice problem 2:
+const cars = ["Tesla","BMW","Toyota","Ford"];
+const newCars = cars.slice(2);
+console.log(newCars);
+
 // practice problem 3:
+const movieList = ["Inception","Titanic","Joker","Avatar","Interstellar"];
+movieList.splice(2,2,"Batman","Superman");
+console.log(movieList);
+
 // practice problem 4:
-// practice problem 5:
-// practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const players = ["Messi","Ronaldo","Neymar","Mbappe"];
+players.splice(2,1,"Halland");
+console.log(players);
+
