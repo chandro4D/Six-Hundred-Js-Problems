a8c9b151a8b6ecf6f3874266e83eabf00ed0effc
chandro4D <chandroshakhor7@gmail.com>
2025-06-13 07:34:12 +0600

    Complete unit13(Practice:5)

--- Unit-Therteen/Practice5.js
+++ Unit-Therteen/Practice5.js
@@ -1,10 +1,18 @@
 // practice problem 1:
+const names = ["Zara","Tariq","Amir","Lina"];
+names.reverse();
+console.log(names);
+
 // practice problem 2:
+const numbers = [1,4,9,7];
+numbers.reverse();
+console.log(numbers);
+
 // practice problem 3:
-// practice problem 4:
-// practice problem 5:
-// practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const users = [
+    {name: "Ali",age: 30},
+    {name: "Sara",age: 25},
+    {name: "Tariq",age: 35}
+];
+users.reverse();
+console.log(users);
