97f3b66cfbc66e55fb47ab8956088af21f905d07
chandro4D <chandroshakhor7@gmail.com>
2025-06-14 10:30:32 +0600

    Complete Unit-14(Practice:01)

--- Unit-Fourteen/Practice1.js
+++ Unit-Fourteen/Practice1.js
@@ -1,10 +1,22 @@
 // practice problem 1:
+const city = "Chattogram";
+console.log(city.indexOf("g"));
+
 // practice problem 2:
+const division = "Sylhet";
+console.log(division.includes("y"));
+
 // practice problem 3:
+const Name = "Rifat";
+const last = Name.length;
+console.log(Name[last-1]);
+
 // practice problem 4:
+const language = "javascript";
+console.log(language.indexOf("rip"));
+
 // practice problem 5:
-// practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const text = "Immutable";
+console.log(text.length);
+text[0] = "C";
+console.log(text);
