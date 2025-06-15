beedb41a013a5dbba9eb3effcabcfae808c2a25e
chandro4D <chandroshakhor7@gmail.com>
2025-06-15 07:53:12 +0600

    Complete Unit-14(Practice:2)

--- Unit-Fourteen/Practice2.js
+++ Unit-Fourteen/Practice2.js
@@ -1,10 +1,23 @@
 // practice problem 1:
+const email = "User@Example.Com";
+console.log(email.toLowerCase());
+
 // practice problem 2:
+const greeting = "hELlo WoRLd";
+console.log(greeting.toLowerCase());
+console.log(greeting.toUpperCase());
+
 // practice problem 3:
+const language = "JavaScript";
+const lowerCase = language.toLowerCase();
+console.log(lowerCase.includes("script"));
+
 // practice problem 4:
-// practice problem 5:
-// practice problem 6:
-// practice problem 7:
-// practice problem 8:
-// practice problem 9:
-// practice problem 10:
+const text = "NodeJs";
+const firstCharacter = text[0];
+if(firstCharacter === firstCharacter.toUpperCase()){
+    console.log("The First Character Is An Upper Case Character");
+}
+else{
+    console.log("The First Character Is Not An Upper Case Character");
+}
