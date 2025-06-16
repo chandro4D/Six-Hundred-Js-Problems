11cf5abf0f49e15d1c6ae4453afa644e002b7cc1
chandro4D <chandroshakhor7@gmail.com>
2025-06-16 08:03:55 +0600

    Complete unit-14(Practice:03)

--- Unit-Fourteen/Practice3.js
+++ Unit-Fourteen/Practice3.js
@@ -1,10 +1,38 @@
 // practice problem 1:
+const friends = "Raju,Rana,Hasan,Kabir,Mahi";
+console.log(friends.split(","));
+
 // practice problem 2:
+const Friends = [ 'Raju', 'Rana', 'Hasan', 'Kabir', 'Mahi' ];
+console.log(Friends.join(","));
+
 // practice problem 3:
+const keyWords = "function if else while";
+console.log(keyWords.split(" "));
+
 // practice problem 4:
+const languages = ["JavaScript","Python","Java"];
+console.log(languages.join(";"));
+
 // practice problem 5:
+const loops = 'for,while,for-in,for-of,do-while';
+console.log(loops.split(","));
+
 // practice problem 6:
+const removeEmptySpace = " console log debug ";
+console.log(removeEmptySpace.trim());
+
 // practice problem 7:
+const str1 = "push";
+const str2 = "pop";
+const str3 = "shift";
+const str4 = "unshift";
+console.log(str1.concat(" ",str2," ",str3," ",str4));
+
 // practice problem 8:
+const Dev = "Hello JavaScript Developers";
+console.log(Dev.slice(6,16));
+
 // practice problem 9:
-// practice problem 10:
+const learn = "Code more learn more";
+console.log(learn.substring(10,16));
