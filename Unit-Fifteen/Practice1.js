4ba5a4f54d2fa9dbfe673b97561ca78f869483f4
chandro4D <chandroshakhor7@gmail.com>
2025-06-19 09:27:02 +0600

    Complete unit-15(Practice:01)

---
+++ Unit-Fifteen/Practice1.js
@@ -0,0 +1,28 @@
+// practice problem 1:
+let num =15;
+let copy = num;
+copy = 25;
+console.log(num,copy);
+
+// practice problem 2:
+let arr = [1,2,3];
+let arr1 = arr;
+arr1.push(44);
+console.log(arr,arr1);
+
+// practice problem 3:
+let language = {
+    name: "JS",
+    age:30
+};
+let newVersion = language;
+newVersion.location = "Browser";
+console.log(language,newVersion);
+
+// practice problem 4:
+
+// practice problem 5:
+
+// practice problem 6:
+
+// practice problem 7:
