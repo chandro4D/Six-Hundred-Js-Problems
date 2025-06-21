e97c1f32a9e8dd718760c08f2317dff6e6b54bf2
chandro4D <chandroshakhor7@gmail.com>
2025-06-22 00:16:36 +0600

    Complete Unit-15

--- Unit-Fifteen/Practice3.js
+++ Unit-Fifteen/Practice3.js
@@ -1,19 +1,29 @@
 // practice problem 1:
-const isLearning = false;
+const isLearning = "false";
 if(isLearning){
     console.log("False is a Truthy Value");
 }
 else{
     console.log("False is a Falsy Value");
 }
+
 // practice problem 2:
+const obj = {};
+if(obj){
+    console.log("Empty object is a truthy Value");
+}
+else{
+    console.log("Empty object is a Falsy Value");
+}
 
 // practice problem 3:
+const arr = {};
+if(arr){
+    console.log("Empty Array is a truthy Value");
+}
+else{
+    console.log("Empty Array is a Falsy Value");
+}
 
 // practice problem 4:
-
-// practice problem 5:
-
-// practice problem 6:
-
-// practice problem 7:
+console.log(!![]);
