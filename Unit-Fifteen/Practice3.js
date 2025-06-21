0e1e21e112007b5d3d468eb10b01d57018714688
chandro4D <chandroshakhor7@gmail.com>
2025-06-21 23:23:55 +0600

    complete unit-15(Practice:3-1)

--- Unit-Fifteen/Practice3.js
+++ Unit-Fifteen/Practice3.js
@@ -1,5 +1,11 @@
 // practice problem 1:
-
+const isLearning = false;
+if(isLearning){
+    console.log("False is a Truthy Value");
+}
+else{
+    console.log("False is a Falsy Value");
+}
 // practice problem 2:
 
 // practice problem 3:
