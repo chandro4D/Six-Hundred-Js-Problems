981944328db43f516d04bef00a29c35d8f4acca9
chandro4D <chandroshakhor7@gmail.com>
2025-06-23 08:58:00 +0600

    complete unit-15(Practice:4)

--- Unit-Fifteen/Practice4.js
+++ Unit-Fifteen/Practice4.js
@@ -1,13 +1,53 @@
 // practice problem 1:
+const isSame = (true == 1);
+if (isSame == true) {
+    console.log("Same");
+}
+else {
+    console.log("Different");
+}
 
 // practice problem 2:
-
+const obj1 = {};
+const obj2 = {};
+if (obj1 === obj2) {
+    console.log("Same");
+}
+else {
+    console.log("Different");
+}
 // practice problem 3:
+let arr1 = [];
+let arr2;
+arr1 = arr2;
+if (arr1 === arr1) {
+    console.log("Same");
+}
+else {
+    console.log("Different");
+}
 
 // practice problem 4:
+function IsSame(num, boolean) {
+    if (num == boolean) {
+        console.log("Equal");
+    }
+    else {
+        console.log("Not Equal");
+    }
+}
+IsSame(9,true);
 
 // practice problem 5:
+if ("" == false) {
+    console.log("Same");
+}
+else {
+    console.log("Different");
+}
 
 // practice problem 6:
+console.log(null === undefined);
 
 // practice problem 7:
+console.log(1 == "1");
