2ba53e7802f2a9afb6c6236b04a49eea5397ab46
chandro4D <chandroshakhor7@gmail.com>
2025-06-24 23:30:04 +0600

    Complete unit-15(Practice:5)

--- Unit-Fifteen/Practice5.js
+++ Unit-Fifteen/Practice5.js
@@ -1,13 +1,40 @@
 // practice problem 1:
+function Name(chandra){
+    chandra()
+}
+function shekhor(){
+    console.log("I Am Chandra Shekhor Mondal");
+}
+Name(shekhor);
 
 // practice problem 2:
+function studentInfo(details){
+    Object.keys(details).forEach(key => {
+        console.log(key);
+    })
+}
+const obj = {
+    name: "Chandra",
+    age: 25,
+    
+}
+studentInfo(obj);
 
 // practice problem 3:
+function numberProcessor(num,callback){
+    const divisionByFive = num / 5 ;
+    callback(divisionByFive);
+}
+function forCallBack(output){
+    console.log("The Result Is:",output);
+}
+numberProcessor(50,forCallBack);
 
 // practice problem 4:
-
-// practice problem 5:
-
-// practice problem 6:
-
-// practice problem 7:
+function WishBirthDay(Wish){
+    Wish()
+}
+function birthday(){
+    console.log("***Happy Birthday Chandra Shekhor Mondal***");
+}
+WishBirthDay(birthday);
