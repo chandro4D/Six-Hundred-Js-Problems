e0bde5a03a2f8f3c582f016b1a11d34cdf5af572
chandro4D <chandroshakhor7@gmail.com>
2025-06-25 11:49:53 +0600

    Complete Unit-15

--- Unit-Fifteen/Practice6.js
+++ Unit-Fifteen/Practice6.js
@@ -1,13 +1,33 @@
 // practice problem 1:
+const taxRate = 15;
+function taxAmount(income){
+    const tax = income * 15/100 ;
+    console.log("Your Tax Amount is:",tax,"Taka");
+}
+taxAmount(30000);
 
 // practice problem 2:
+function secret(){
+    let insideSecret = "Internal Secret Hiding Place";
+}
+/* ReferenceError: insideSecret is not defined */
+// console.log(insideSecret);
+
 
 // practice problem 3:
+if(true){
+    let temperature = 100;
+}
+/* ReferenceError: temperature is not defined */
+// console.log(temperature);
+
 
 // practice problem 4:
-
-// practice problem 5:
-
-// practice problem 6:
-
-// practice problem 7:
+function schoolDetails(){
+    const schoolName = "Bedkashi Collegiate School" ;
+    function displaySchoolName(){
+        console.log(schoolName);
+    }
+    displaySchoolName();
+}
+schoolDetails();
