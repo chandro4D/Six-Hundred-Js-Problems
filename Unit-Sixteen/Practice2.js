172d8e24f2055541c23cca06bb82c512de28b02b
chandro4D <chandroshakhor7@gmail.com>
2025-06-27 10:07:31 +0600

    Complete unit-16(Practice:2)

--- Unit-Sixteen/Practice2.js
+++ Unit-Sixteen/Practice2.js
@@ -1,13 +1,28 @@
 // practice problem 1:
+console.log("The Minimum Number Is:",Math.min(45,11,89,23,56,-12,-57));
 
 // practice problem 2:
+console.log("The Maximum Number Is:",Math.max(21,35,67));
 
 // practice problem 3:
+console.log("Round Number-1:",Math.round(7.6));
+console.log("Round Number-2:",Math.round(7.2));
 
 // practice problem 4:
+console.log("Floor Number-1:",Math.floor(9.8));
+console.log("Floor Number-2:",Math.floor(5.3));
 
 // practice problem 5:
+console.log("Ceil Number-1:",Math.ceil(3.1));
+console.log("Ceil Number-2:",Math.ceil(6.9));
 
 // practice problem 6:
+console.log("Absolute Value Is:",Math.abs(-34));
 
 // practice problem 7:
+function mathOperation(num){
+    console.log(Math.round(num));
+    console.log(Math.floor(num));
+    console.log(Math.ceil(num));
+}
+mathOperation(3.60);
