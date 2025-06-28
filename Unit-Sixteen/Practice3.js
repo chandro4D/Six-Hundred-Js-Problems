9168993737c32c7f12139fb2a602fb1642bb6643
chandro4D <chandroshakhor7@gmail.com>
2025-06-28 15:41:18 +0600

    Complete unit-16(Practice:3)

--- Unit-Sixteen/Practice3.js
+++ Unit-Sixteen/Practice3.js
@@ -1,13 +1,23 @@
 // practice problem 1:
+const nowDate = new Date();
+console.log("Now Date:",nowDate);
 
 // practice problem 2:
+const date = new Date(2035,6,15,14,45,30);
+console.log(date);
 
 // practice problem 3:
+const fullYear =new Date().getFullYear();
+console.log(fullYear);
 
 // practice problem 4:
+const setYear = new Date();
+setYear.setFullYear(2040);
+console.log(setYear.getFullYear());
 
 // practice problem 5:
+const day =new Date();
+day.setDate(2029-2-16);
+console.log(day.getDay());
 
-// practice problem 6:
 
-// practice problem 7:
