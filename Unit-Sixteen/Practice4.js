5ae740a3cd5988a4bdacbaf2ee04a8eb728a860d
chandro4D <chandroshakhor7@gmail.com>
2025-06-29 23:40:26 +0600

    complete unit-16(Practice:04)

--- Unit-Sixteen/Practice4.js
+++ Unit-Sixteen/Practice4.js
@@ -1,8 +1,17 @@
 // practice problem 1:
+const sentence = "I bought an orange";
+const changeSen = sentence.replace(/orange/g,"grape");
+console.log(changeSen);
 
 // practice problem 2:
+const sentence1 = "I like to have apple and banana";
+const pattern = /ana/ ;
+console.log(pattern.test(sentence1));
 
 // practice problem 3:
+const sentence2 = "I am eating apple.apple is good.apple helps me a lot.";
+const changeSen1 = sentence2.replace(/apple/g,"JavaScript");
+console.log(changeSen1);
 
 // practice problem 4:
 
