5285e9fac0b9a474fb1d7ed5540a478bbb3312de
chandro4D <chandroshakhor7@gmail.com>
2025-07-03 12:40:23 +0600

    Complete unit-17

--- Unit-Seventeen/Practice2.js
+++ Unit-Seventeen/Practice2.js
@@ -1,13 +1,11 @@
 // practice problem 1:
+/* "ANS: SyntaxError"*/
 
 // practice problem 2:
+/* "Reference Error Occurs When we use a variable which is uninitialized " */
 
 // practice problem 3:
+/* When we write wrong syntax code like missing bracket or semiclone
+then we face Syntax error problem..And Type error occurs when we use
+one operation which is not for that types of data.*/
 
-// practice problem 4:
-
-// practice problem 5:
-
-// practice problem 6:
-
-// practice problem 7:
