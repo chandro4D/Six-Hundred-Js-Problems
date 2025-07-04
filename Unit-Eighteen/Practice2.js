6a1b1235c4efc45c1c5753f5027e4ff2509c7285
chandro4D <chandroshakhor7@gmail.com>
2025-07-05 01:20:40 +0600

    Complete unit-18(Practice:2)

--- Unit-Eighteen/Practice2.js
+++ Unit-Eighteen/Practice2.js
@@ -1,13 +1,25 @@
 // practice problem 1:
+setTimeout(()=>{
+    console.log("I wasted 3 seconds of my life by looking at screen and doing nothing");
+},3000);
 
 // practice problem 2:
+let num = 129;
+setInterval(()=>{
+    num += 2;
+    console.log(num);
+},2000);
 
 // practice problem 3:
+let Num = 0;
+const intervalId = setInterval(()=>{
+    Num++;
+    console.log("I am learning javascript");
+    if(Num == 6){
+        clearInterval(intervalId);
+    }
+},2000)
 
 // practice problem 4:
-
-// practice problem 5:
-
-// practice problem 6:
-
-// practice problem 7:
+   /* If we remove second parameter of setTimeout(),it take 0 millisecond by default.
+   JavaScript doesn’t support microsecond level timing.. */
