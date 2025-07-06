e9d7ffc8d673df075db9da320315dc0f92ac20c2
chandro4D <chandroshakhor7@gmail.com>
2025-07-06 23:37:33 +0600

    Complete unit-18(Practice:3)

--- Unit-Eighteen/Practice3.js
+++ Unit-Eighteen/Practice3.js
@@ -1,13 +1,10 @@
 // practice problem 1:
+/* Even Loop works on mainly the two main things, Call Stack & Callback Queue */
 
 // practice problem 2:
+/* For Synchronous works Even Loop use call Stack and for Asynchronous task Even Loop 
+use Callback Queue */
 
 // practice problem 3:
-
-// practice problem 4:
-
-// practice problem 5:
-
-// practice problem 6:
-
-// practice problem 7:
+/*JavaScript is single threated,But it handle asynchronous 
+task with the help of Even loop */
