a3f1b6eb51784c067eaac93ae31680ce34cd5c40
chandro4D <chandroshakhor7@gmail.com>
2025-07-08 22:50:14 +0600

    Copmplete unit-18

--- Unit-Eighteen/Practice4.js
+++ Unit-Eighteen/Practice4.js
@@ -1,5 +1,5 @@
 // practice problem 1:
-
+    /*JavaScript code run on the browser with the help of V8 Engine */
 // practice problem 2:
     /* JavaScript is a high level programming Language */
 // practice problem 3:
@@ -8,6 +8,3 @@
 
 // practice problem 5:
 
-// practice problem 6:
-
-// practice problem 7:
