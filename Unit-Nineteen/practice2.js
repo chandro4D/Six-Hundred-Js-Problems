9a430ff75c3d1036f39ff4a3fae0b4fb94c99de5
chandro4D <chandroshakhor7@gmail.com>
2025-07-11 18:21:37 +0600

    complete--unit-19(Practice:2)

---
+++ Unit-Nineteen/practice2.js
@@ -0,0 +1,22 @@
+// practice problem 1:
+const getUser = new Promise((resolve,reject)=>{
+    const usersAvailable = true;
+    const users = ["Chandra","Shekhor","Mondal","Bindu"];
+    if(usersAvailable){
+        resolve(users);
+    }else{
+        reject("No User Data")
+    }
+})
+
+// practice problem 2:
+
+
+// practice problem 3:
+
+
+// practice problem 4:
+
+
+// practice problem 5:
+
