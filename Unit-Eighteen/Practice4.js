86b76e58aae960aa852c5d5e52760432a0042eef
chandro4D <chandroshakhor7@gmail.com>
2025-07-16 08:12:04 +0600

    Complete unit-18 & 19

--- Unit-Eighteen/Practice4.js
+++ Unit-Eighteen/Practice4.js
@@ -1,7 +1,11 @@
 // practice problem 1:
-    /*JavaScript code run on the browser with the help of V8 Engine */
+    /*JavaScript code run on the browser with the help of V8 Engine.At first V8 Engine parse Js code,
+       then Interpreter read the code,after that JIT(Just In Time) came and then some
+       parts of code transformed into Machine code which run code first and give the output */
+
 // practice problem 2:
-    /* JavaScript is a high level programming Language */
+    /* JavaScript is a high level programming Language.*/
+
 // practice problem 3:
 
 // practice problem 4:
