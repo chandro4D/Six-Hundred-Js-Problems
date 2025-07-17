524ed5e661f114ccffdadf4b3de4fe20474f2c67
chandro4D <chandroshakhor7@gmail.com>
2025-07-17 23:27:42 +0600

    Complete Unit-18

--- Unit-Eighteen/Practice4.js
+++ Unit-Eighteen/Practice4.js
@@ -7,8 +7,24 @@
     /* JavaScript is a high level programming Language.*/
 
 // practice problem 3:
+function add(a, b) {
+  return a + b;
+}
+console.log("The Sum Is:",add(22, 5)); 
 
 // practice problem 4:
+                           /*memory management*/
+  /*In JavaScript, memory management is automatic, meaning the engine allocates 
+    memory when variables, objects, or functions are created. As the program runs, 
+    this memory is used to store data and perform operations. Developers don’t need to 
+    manually allocate or free memory, as JavaScript handles this process internally.
 
-// practice problem 5:
+                            /*Garbage collection*/
+  /*Garbage collection is the process of automatically freeing memory that is no longer 
+    in use. The most common method is the mark-and-sweep algorithm, which removes values 
+    that are no longer reachable in the code. When there are no references to an object, 
+    the garbage collector clears it to reclaim memory. This helps prevent memory leaks 
+    and keeps the application efficient.
+*/
+
 
