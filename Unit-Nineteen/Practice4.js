56e221451b367d8d09cf6979c21f515df00bd430
chandro4D <chandroshakhor7@gmail.com>
2025-07-19 07:57:23 +0600

    Complete unit-19(Practice:4)

--- Unit-Nineteen/Practice4.js
+++ Unit-Nineteen/Practice4.js
@@ -1,13 +1,33 @@
 // practice problem 1:
+    /*
+      For change profile single or married like data , it is a PATCH operation ,because we 
+      only change one or two things not all parts.
+    */
 
 // practice problem 2:
+    /*
+      For add new information into database we use POST HTTP method.
+    */
 
 // practice problem 3:
+    /*
+      When we see video from a website , then GET operation is used.
+    */
 
 // practice problem 4:
+    /*
+      CRUD is the first letter of Create,Read,Update and Delete.When we works on data CRUD
+      operation is very necessary for that time.
+    */
 
 // practice problem 5:
-
+    /*
+       There are four HTTP method or verb.They are POST,GET,UPDATE & DELETE.
+       GET/Read--->Used for get or retrieve data from the server.
+       POST/Create--->Used for add new data to the server.
+       UPDATE/PUT/PATCH--->Used for update data.
+       DELETE---------->Used for remove data from the server.
+    */
 // practice problem 6:
 
 // practice problem 7:
