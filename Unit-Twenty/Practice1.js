4c0ff89cd5512187d856f72204882a490513d1d0
chandro4D <chandroshakhor7@gmail.com>
2025-07-24 23:26:08 +0600

    Complete unit-20(Practice:1-->1&2)

--- Unit-Twenty/Practice1.js
+++ Unit-Twenty/Practice1.js
@@ -1,6 +1,16 @@
 // practice problem 1:
+const array = [{id:1,name:"mukta",address:"mirpur"}]
+console.log(array[0].address);
 
 // practice problem 2:
+const library ={
+    name:"city library",
+    books:[
+        {id:1,title:"JavaScript Basics",price:300},
+        {id:2,title:"Python Essentials",price:500}
+    ]
+}
+console.log(library.books[1].price);
 
 // practice problem 3 :
 
