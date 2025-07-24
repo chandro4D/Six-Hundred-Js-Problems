f0c6f37609005d1a1f9e181363884719240ba50d
chandro4D <chandroshakhor7@gmail.com>
2025-07-25 00:51:46 +0600

    Complete unit-20(Practice:1)

--- Unit-Twenty/Practice1.js
+++ Unit-Twenty/Practice1.js
@@ -13,17 +13,75 @@ const library ={
 console.log(library.books[1].price);
 
 // practice problem 3 :
+const school = {
+    name:"Green High",
+    students:[
+    {
+        id:1,
+        name:"Samiha"
+    },
+    {
+        id:2,
+        name:"Kamal"
+    }
+]
+}
+console.log(school.students[0].name);
 
 // practice problem 4 :
+const shop = {
+    items:[
+    {
+        name:"pen",
+        stock:100
+    },
+    {
+        name:"notebook",
+        stock:50
+    }
+]
+}
+console.log(shop.items[1].stock);
 
 // practice problem 5 :
+const movie = {
+    title:"Inception",
+    director:{
+        name:"Nolan",
+        age:50
+    },
+    rating: 8.8
+}
+console.log(movie.director.name);
 
 // practice problem 6 :
+const game = {
+    name:"Football",
+    players: [
+    {
+        id:1,
+        name:"Lionel Messi"
+    },
+    {
+        id:2,
+        name:"Cristiano Ronaldo"
+    }
+]
+}
+console.log(game.players[0].name);
 
 // practice problem 7 :
+const vehicle = {
+    type:"car",
+    features: 
+    {
+        color:"Red",
+        brand: 
+        {
+            name:"Toyota",
+            model:"Corolla"
+        }
+    }
+}
+console.log(vehicle.features.brand.name);
 
-// practice problem 8 :
-
-// practice problem 9 :
-
-// practice problem 10:
