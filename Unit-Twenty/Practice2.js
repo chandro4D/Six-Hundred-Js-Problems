54e30915a65b7e7cc9c18407c56f7915b57018cd
chandro4D <chandroshakhor7@gmail.com>
2025-07-25 23:20:08 +0600

    Complete unit-20(Practice:2)

--- Unit-Twenty/Practice2.js
+++ Unit-Twenty/Practice2.js
@@ -1,19 +1,30 @@
 // practice problem 1:
+let x = null;
+console.log(x??=75);
+       // output is : 75(Because x is null)
 
 // practice problem 2:
+const product = {name:"Smart Watch",Price: 100}
+console.log(product?.stock??0);
 
 // practice problem 3 :
-
+const productDetails = {name:"Smart Watch",Price: 100,discount:0};
+const Discount = productDetails.discount || 10;
+console.log(Discount);
 // practice problem 4 :
+const vehicle = {name:"Toyota",version:2020,engine:null};
+console.log(vehicle?.engine??"Engine info missing");
 
 // practice problem 5 :
+const restaurant = {name:"Fathers Restaurant"};
+console.log(restaurant?.menu??"Menu Not Available");
 
 // practice problem 6 :
+const profile = {
+    social:{
+        facebook: 23
+    }
+}
+console.log(profile?.social?.twitter??"Twitter handle not available");
 
-// practice problem 7 :
 
-// practice problem 8 :
-
-// practice problem 9 :
-
-// practice problem 10:
