493defa0a2fb5750d2f368558880918a27e345fa
chandro4D <chandroshakhor7@gmail.com>
2025-07-26 18:30:39 +0600

    Complete unit-20(Practice:3-->1-5)

--- Unit-Twenty/Practice3.js
+++ Unit-Twenty/Practice3.js
@@ -1,13 +1,40 @@
 // practice problem 1:
+let a = 59;
+a = a++;
+console.log(a);
+a = ++a;
+console.log(a);
 
 // practice problem 2:
+let orange = 100;
+orange /= 15;
+console.log(orange);
 
 // practice problem 3 :
+let mango = 20;
+mango &&= 10;
+console.log(mango);
+   /*
+        The new variable value is 10.Because, When we use AND assignment operator(&&=) 
+        for a truthy value then it`s value change .
+   */
 
 // practice problem 4 :
+let bananas = 50;
+bananas *= 4;
+console.log(bananas);
+    /*
+        I use *= this assignment operator.
+    */
 
 // practice problem 5 :
-
+let grapes = 19;
+grapes ||= 10;
+console.log(grapes);
+   /*
+        The  variable value is 19.Because, When we use ORassignment operator(||=) 
+        for a truthy value then it`s value unchanged.
+   */
 // practice problem 6 :
 
 // practice problem 7 :
