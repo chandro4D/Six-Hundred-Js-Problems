d203305836e7505d5a3c67140b1415d11243c406
chandro4D <chandroshakhor7@gmail.com>
2025-07-28 22:46:21 +0600

    Complete unit-21 some

---
+++ Unit-Twenty-One/Practice1.js
@@ -0,0 +1,18 @@
+// practice problem 1:
+  /*
+     A class is a blueprint of template for creating object.It defines properties and
+     behavior.An object is an instance of a class that contains actual values.
+  */
+// practice problem 2:
+
+// practice problem 3 :
+
+// practice problem 4 :
+
+// practice problem 5 :
+
+// practice problem 6 :
+
+// practice problem 7 :
+
+
