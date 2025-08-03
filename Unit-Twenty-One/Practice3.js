4d78eaca607d2e19f476d82394eb36c001eae0cc
chandro4D <chandroshakhor7@gmail.com>
2025-08-03 23:40:09 +0600

    Complete unit-21(Practice:3{1/2})

--- Unit-Twenty-One/Practice3.js
+++ Unit-Twenty-One/Practice3.js
@@ -1,7 +1,18 @@
 // practice problem 1:
+    /*
+        Inheritance in js is a feature that allows one class (Child class) to use properties
+        and methods of another class(Parent Class). We need inheritance to follow the DRY 
+        (Don't Repeat Yourself) principle and make code easier to manage and scale.
+    */
 
 // practice problem 2:
+    /*
+        In JavaScript, use the `extends` keyword to make a child class inherit 
+        from a parent class, and use `super()` inside the child constructor to 
+        call the parent's constructor.
 
+    */
+   
 // practice problem 3 :
 
 // practice problem 4 :
