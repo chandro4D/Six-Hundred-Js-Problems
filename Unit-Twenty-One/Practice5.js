8e6948c451f48bc3211923a07e633acc04ade9b1
chandro4D <chandroshakhor7@gmail.com>
2025-08-14 22:49:27 +0600

    add some

--- Unit-Twenty-One/Practice5.js
+++ Unit-Twenty-One/Practice5.js
@@ -17,3 +17,29 @@
 // practice problem 9 :
 
 // practice problem 10:
+// practice problem 1:
+    /*
+        In JavaScript, inheritance works through prototypes. Every object can inherit 
+        properties and methods from another object using the prototype chain. 
+    */
+
+// practice problem 2:
+    /*
+        Prototypical inheritance in JavaScript is a feature where objects inherit 
+        properties and methods from other objects through a prototype chain. Instead 
+        of classes, JavaScript uses prototypes, allowing one object to serve as a template 
+        for another.
+    */
+    
+// practice problem 3 :
+const person = {
+    name: "Chandra Shekhor Mondal"
+}
+console.log(Object.getPrototypeOf(person));
+console.log(person.toString);
+
+// practice problem 4 :
+    /*
+        If not have my object property name toString ,then also i use toString method 
+        because in my object prototype have toString method.
+    */
