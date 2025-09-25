ce99fa477aae0e7fbc3bf4c15245a2691453b0c6
chandro4D <chandroshakhor7@gmail.com>
2025-09-25 23:35:17 +0600

    Complete unit-23(Practice-01)

--- Unit-Twenty-Two/Practice5.js
+++ Unit-Twenty-Two/Practice5.js
@@ -2,11 +2,23 @@
 commentNums++;
 let commentNums = 20;
 console.log(commentNums);
+// Get this "ReferenceError: Cannot access 'commentNums' before initialization" error
 
 // practice problem 2:
+views/2;
+const views = 500;
+console.log(views);
+// Get this "ReferenceError: Cannot access 'views' before initialization" error
 
 // practice problem 3 :
-
+ 
+   /*
+      The Temporal Dead Zone (TDZ) in JavaScript is the time between when a `let` or `const` variable is 
+      hoisted and when it’s initialized. Accessing the variable during this period throws a **ReferenceError**. 
+      It’s important because it prevents accidental use of variables before declaration, making code safer and 
+      more predictable.
+   */
+  
 // practice problem 4 :
 
 // practice problem 5 :
