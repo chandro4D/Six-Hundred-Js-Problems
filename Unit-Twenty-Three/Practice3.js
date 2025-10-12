11bf16363024b096fee75ec38fd58e45b714e757
chandro4D <chandroshakhor7@gmail.com>
2025-10-12 20:07:11 +0600

    Update unit-23(Practice-03)

--- Unit-Twenty-Three/Practice3.js
+++ Unit-Twenty-Three/Practice3.js
@@ -1,11 +1,16 @@
 // practice problem 1:
-
+  /*
+    With is a javascript statement which is used for set javascript scope willingly.
+    It is not recommended to use it in modern javascript because it makes code hard to
+    read and debug.
+  */
 
 // practice problem 2:
-
-
-// practice problem 3 :
-
-
-// practice problem 4 :
+  /*
+    In JavaScript, eval() executes a string as JavaScript code. It is not recommended 
+    because it poses serious security risks, allowing attackers to run harmful code if 
+    user input is passed. It also slows performance since it forces the interpreter to 
+    re-parse code at runtime. Instead, safer alternatives like functions or `JSON.parse()` 
+    should be used.
+  */
 
