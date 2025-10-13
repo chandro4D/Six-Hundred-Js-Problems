046dd24a0d1e4437752ad09e31270f326d2bce5f
chandro4D <chandroshakhor7@gmail.com>
2025-10-13 18:19:40 +0600

    Complete unit-23

---
+++ Unit-Twenty-Three/Practice5.js
@@ -0,0 +1,9 @@
+// practice problem 1:
+
+    /*
+        Strict mode in JavaScript is used to make the code more secure and easier to debug. 
+        It helps catch common coding mistakes, such as using undeclared variables. 
+        It also prevents the use of certain unsafe features, like deleting variables or 
+        using reserved keywords. By enabling strict mode, developers can write cleaner, 
+        more reliable and maintainable code.
+    */
