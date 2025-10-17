5281684077e58828969b7cc372c946c81ec88439
chandro4D <chandroshakhor7@gmail.com>
2025-10-17 23:14:42 +0600

    Complete unit->24

--- Unit-Twenty-Five/Practice1.js
+++ Unit-Twenty-Five/Practice1.js
@@ -1,11 +1,14 @@
 // practice problem 1:
+
    /*
        JavaScript code runs in the browser using javaScript engine(V8),that interprets and executes the code.
        At first the engine parsing the code and then it compiles the code to machine code and 
        finally it executes the code.
    */
 
+
 // practice problem 2:
+
     /*
         The V8 engine is Google’s open-source JavaScript engine used in Chrome and Node.js. It compiles 
         JavaScript code directly into machine code for faster execution. It also manages memory and optimizes 
@@ -13,7 +16,9 @@
 
     */
 
+
 // practice problem 3:
+
     /*
         A JIT (Just-In-Time) compiler improves JavaScript performance by compiling code at runtime instead of 
         before execution. It first interprets the code then identifies frequently used parts and compiles them 
