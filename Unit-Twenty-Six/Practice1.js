52f62c8388bdaa8c039793d46c686a789741328f
chandro4D <chandroshakhor7@gmail.com>
2025-10-19 20:56:39 +0600

    Complete unit-26(Task-01-->03)

--- Unit-Twenty-Six/Practice1.js
+++ Unit-Twenty-Six/Practice1.js
@@ -29,7 +29,8 @@ function addToCart(productId, quantity) {
     let productInfo = {
         id: productId,
         quantity: quantity,
-        price: product.price
+        price: product.price,
+        name: product.name
     };
     cart.push(productInfo);
 
@@ -39,7 +40,35 @@ function addToCart(productId, quantity) {
 }
 addToCart(1,2);
 
+
 // Task----02:
+function viewCart() {
+    cart.forEach(item => {
+        console.log(`Product Name: ${item.name}, Product ID: ${item.id}, Quantity: ${item.quantity}, Price per unit: ${item.price}`);
+    })
+}
+viewCart();
 
 // Task----03:
-// Task----04:
+function placeOrder() {
+    if(cart.length === 0){
+        return "Your cart is empty";
+    }
+    else{
+        cart.forEach(item => {
+            let totalPrice = item.price * item.quantity;
+            console.log(totalPrice);
+            let totalOrder = {
+                id: item.id,
+                name: item.name,
+                quantity: item.quantity,
+                totalPrice: totalPrice
+            };
+            orders.push(totalOrder);
+        })
+        cart.length = 0; // Clear the cart after placing the order
+        return "Order placed successfully";
+    }
+}
+placeOrder();
+console.log(orders)
