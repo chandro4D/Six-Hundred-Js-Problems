92bbdaa6046e15f6e563acb6b2504305321dbef4
chandro4D <chandroshakhor7@gmail.com>
2025-10-20 23:50:58 +0600

    Complete some 26 units tasks

---
+++ Unit-Twenty-Six/Practice2.js
@@ -0,0 +1,14 @@
+const restaurants = [
+    {id:1, name: "Pizza Place", menu: [{name: "Pepperoni Pizza",price: 12}]},
+    {id:2, name: "Sushi House", menu: [{name: "Salmon Sushi",price: 15}]}
+];
+const orders = [];
+const reviews = [];
+//Discount in percentage
+const discountCodes = {"SAVE10":10, "WELCOME15":15};
+
+// Task----01:
+// Task----02:
+// Task----03:
+// Task----04:
+// Task----05:
