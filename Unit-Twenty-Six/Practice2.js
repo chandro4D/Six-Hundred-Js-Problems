fe2e5341b19b3f96beee5c2d79be15ede9350e6e
chandro4D <chandroshakhor7@gmail.com>
2025-10-21 12:17:36 +0600

    Complete unit-26(Task-01&02)

--- Unit-Twenty-Six/Practice2.js
+++ Unit-Twenty-Six/Practice2.js
@@ -1,14 +1,63 @@
 const restaurants = [
-    {id:1, name: "Pizza Place", menu: [{name: "Pepperoni Pizza",price: 12}]},
-    {id:2, name: "Sushi House", menu: [{name: "Salmon Sushi",price: 15}]}
+    { id: 1, name: "Pizza Place", menu: [{ name: "Pepperoni Pizza", price: 12 }] },
+    { id: 2, name: "Sushi House", menu: [{ name: "Salmon Sushi", price: 15 }] }
 ];
 const orders = [];
 const reviews = [];
-//Discount in percentage
-const discountCodes = {"SAVE10":10, "WELCOME15":15};
+/* Discount in percentage */
+const discountCodes = { "SAVE10": 10, "WELCOME15": 15 };
 
-// Task----01:
+
+// Task----01: 
+function getFoodPrice(restaurantName, foodName) {
+    const restaurant = restaurants.find(n => n.name === restaurantName);
+
+    if (!restaurant) {
+        return "Restaurant not found";
+    }
+    const foodItem = restaurant.menu.find(i => i.name === foodName);
+    if (!foodItem) {
+        return "Food Item Not Found"
+    } else {
+        return foodItem.price
+    }
+}
+
+getFoodPrice("Pizza Place","Pepperoni Pizza");
+
 // Task----02:
+function placeOrder(customer, restaurantName, foodItem, discountCode) {
+    let foodPrice = getFoodPrice(restaurantName, foodItem);
+
+    if (!foodPrice) {
+        console.log("Food Item Not Found")
+    };
+    console.log(foodPrice);
+
+    if (discountCode in discountCodes) {
+        foodPrice = foodPrice - (foodPrice * discountCodes[discountCode] / 100);
+        console.log(foodPrice);
+    } else {
+        console.log("Invalid Discount Code");
+    }
+
+
+    const order = {
+        id: orders.length + 1,
+        customer: customer,
+        restaurant: restaurantName,
+        foodItem: foodItem,
+        price: foodPrice,
+        status: "Pending"
+    };
+    orders.push(order);
+    console.log(`Order placed successfully: \n${JSON.stringify(order)} \nOrder ID: ${order.id}`);
+
+
+}
+placeOrder("Chandra Shekhor Mondal", "Pizza Place", "Pepperoni Pizza", "WELCOME15");
+
+
 // Task----03:
 // Task----04:
 // Task----05:
