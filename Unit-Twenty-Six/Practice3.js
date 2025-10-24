a9c62aa2b56fde8ad8dc2e987cabcbba30d1fc9c
chandro4D <chandroshakhor7@gmail.com>
2025-10-24 22:38:09 +0600

    Complete unit-26(Practice-3-task01)

---
+++ Unit-Twenty-Six/Practice3.js
@@ -0,0 +1,33 @@
+const userDatabase = (function(){
+    const users = [];
+    return {
+        registerUser: function(name,pin){
+            if(!name || !pin){
+                return console.error("Please! Give Your Name & Pin"); 
+            }
+            if(typeof pin !== "number" || String(pin).length !== 4){
+                return console.error("Please give Pin As A Number And Ensure the Pin is 4 digit");
+                
+            };
+            const newUser = {
+                id: users.length+1,
+                name: name,
+                pin: pin,
+                balance: 0
+            }
+            users.push(newUser);
+            return true;
+        }
+    };
+})();
+userDatabase.registerUser("Chandra", 1234);
+
+
+
+// Task:->01
+
+// Task:->02
+// Task:->03
+// Task:->04
+// Task:->05
+// Task:->06
