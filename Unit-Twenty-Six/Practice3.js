2955668a921b1ef7c9826564e8ed3ba392e219b1
chandro4D <chandroshakhor7@gmail.com>
2025-10-26 21:55:49 +0600

    Complete unit-26(Task--02)

--- Unit-Twenty-Six/Practice3.js
+++ Unit-Twenty-Six/Practice3.js
@@ -1,29 +1,38 @@
-const userDatabase = (function(){
+const userDatabase = (function () {
     const users = [];
     return {
-        registerUser: function(name,pin){
-            if(!name || !pin){
-                return console.error("Please! Give Your Name & Pin"); 
+        registerUser: function (name, pin) {
+            if (!name || !pin) {
+                return console.error("Please! Give Your Name & Pin");
             }
-            if(typeof pin !== "number" || String(pin).length !== 4){
+            if (typeof pin !== "number" || String(pin).length !== 4) {
                 return console.error("Please give Pin As A Number And Ensure the Pin is 4 digit");
-                
+
             };
             const newUser = {
-                id: users.length+1,
+                id: users.length + 1,
                 name: name,
                 pin: pin,
                 balance: 0
             }
             users.push(newUser);
+            console.log("Register New User:",newUser)
             return true;
         },
-        loginUser: function(name,phoneNum){
-            
+        loginUser: function (name, Pin) {
+            if (users.find(p => p.pin === Pin)){
+                console.log("Log In Successfully")
+                return true;
+            }  
+            else{
+                console.log("Your Pin Is Incorrect")
+                return false;
+            }         
         }
     };
 })();
-userDatabase.registerUser("Chandra", 1234);
+userDatabase.registerUser("Chandra",1234);
+userDatabase.loginUser("Chandra", 1234);
 
 
 
