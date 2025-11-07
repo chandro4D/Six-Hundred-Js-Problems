9f3b6783ccbf4daac22b6e540934d7ec71285991
chandro4D <chandroshakhor7@gmail.com>
2025-11-07 23:29:40 +0600

    Complete unit-25(Practice-04__Task-06)

--- Unit-Twenty-Six/Practice4.js
+++ Unit-Twenty-Six/Practice4.js
@@ -1,39 +1,43 @@
-class TodoApp{
-    constructor(todos){
-        this.todos = todos;
+class TodoApp {
+  constructor(todos) {
+    this.todos = todos;
+  }
+  addTodo(TaskName, TaskDuration, TaskCategory) {
+    const task = {
+      id: todos.length + 1,
+      TaskName: TaskName,
+      TaskDuration: TaskDuration,
+      TaskCategory: TaskCategory,
+      completed: false
     }
-    addTodo(TaskName,TaskDuration,TaskCategory){
-        const task = {
-            id: todos.length + 1,
-            TaskName: TaskName,
-            TaskDuration: TaskDuration,
-            TaskCategory: TaskCategory,
-            completed: false
-        }
-        todos.push(task);
+    todos.push(task);
+  }
+  removeTodo(taskName) {
+    const findTask = todos.find(task => task.TaskName === taskName);
+    const TaskIndex = todos.indexOf(findTask);
+    todos.splice(TaskIndex, 1);
+
+  }
+  completeTodo(taskName) {
+    const findTask = todos.find(task => task.TaskName === taskName);
+    if (!findTask) {
+      return false;
     }
-    removeTodo(taskName){
-      const findTask = todos.find(task => task.TaskName === taskName);
-      const TaskIndex = todos.indexOf(findTask);
-      todos.splice(TaskIndex,1);
-      
+    else {
+      findTask.completed = true;
+      return true;
     }
-    completeTodo(taskName){
-      const findTask = todos.find(task => task.TaskName === taskName);
-      if(!findTask){
-        return false;
-      }
-      else{
-        findTask.completed = true;
-        return true;
-      }
+  }
+  displayTodoList(taskCategory) {
+    if (!taskCategory) {
+      console.log(todos)
+    } else {
+      const findTask = todos.filter(task => task.TaskCategory === taskCategory);
+      console.log(findTask);
     }
-    displayTodoList(taskCategory){
-      if(!taskCategory){
-        console.log(todos)
-      }else{
-        const findTask = todos.filter(task => task.TaskCategory === taskCategory);
-        console.log(findTask);
-      }
-    }
+  }
+  hoursWorked() {
+    const totalTime = todos.reduce((sun,task) => sum + task.TaskDuration,0 );
+    console.log(totalTime);
+  }
 }
